ae5d23ac75d13458cf32227ef7476be05936c46c
stehames <stehh.amess@gmail.com>
2021-10-13 10:18:35 -0300

    Commit da aula 02/10/2021, configuração do projeto

---
+++ src/app.js
@@ -0,0 +1,24 @@
+//comand js
+//const express = require('express');
+
+import express from 'express';
+import routes from './routes';
+
+class App{
+    constructor(){
+        this.server = express();
+
+        this.middleware();
+        this.routes();
+    }
+
+    middleware(){
+        this.server.use(express.json());
+    }
+
+    routes(){
+        this.server.use(routes);
+    }
+}
+
+export default new App().server;
---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: "mysql",
+    host: "localhost",
+    username: "",
+    password: "",
+    database: "",
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+
+};
